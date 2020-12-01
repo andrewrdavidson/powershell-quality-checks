a45c806aaa3c263bd17c558fe471aca6fdd9b3e7
Andrew Davidson <github@kelrys.com>
2020-12-01 21:45:44 +0000

    First release of 'built' versions of the modules

--- Modules/Common.Pester.Functions.psm1
+++ Modules/Common.Pester.Functions.psm1
@@ -19,7 +19,7 @@ function Export-FunctionsFromModule {
     )
 
     # Get the file properties of our module
-    $fileProperties = (Get-ChildItem -LiteralPath $Path)
+    $fileProperties = (Get-Item -LiteralPath $Path)
     $moduleName = $fileProperties.BaseName
 
     # Generate a new temporary output path for our extracted functions
@@ -94,6 +94,73 @@ function Export-FunctionsFromModule {
 
 }
 
+function Get-FileContent {
+    <#
+        .DESCRIPTION
+        Description of TestScript
+
+        .SYNOPSIS
+        A detailed synopsis of the function of the script
+
+        .PARAMETER File
+        A string containing the message
+
+        .EXAMPLE
+        Get-TestFunction -Message 'This is a test message'
+    #>
+    [CmdletBinding()]
+    [OutputType([System.String[]])]
+    param (
+        [string]$File
+    )
+
+    $fileContent = Get-Content -Path $File
+
+    $parserErrors = $null
+    $parsedFileFunctions = [System.Management.Automation.PSParser]::Tokenize($fileContent, [ref]$parserErrors)
+
+    # Create an array of where each reference of the keyword 'function' is
+    $parsedFunctions = ($parsedFileFunctions | Where-Object { $_.Type -eq "Keyword" -and $_.Content -like 'function' })
+
+    if ($parsedFunctions) {
+
+        foreach ($function in $parsedFunctions) {
+
+            $startLine = ($function.StartLine)
+
+            for ($line = $fileContent.Count; $line -gt $function.StartLine; $line--) {
+
+                if ($fileContent[$line] -like "}") {
+
+                    $endLine = $line
+                    break
+
+                }
+
+            }
+
+            # Output the lines of the function to the FunctionOutputFile
+            for ($line = $startLine; $line -lt $endLine; $line++) {
+                $parsedFileContent += $fileContent[$line]
+                $parsedFileContent += "`n"
+            }
+
+        }
+
+    }
+    else {
+
+        for ($line = 0; $line -lt $fileContent.Count; $line++) {
+            $parsedFileContent += $fileContent[$line]
+            $parsedFileContent += "`n"
+        }
+
+    }
+
+    return $parsedFileContent
+
+}
+
 function Get-FileList {
     <#
         .DESCRIPTION
@@ -130,6 +197,7 @@ function Get-FileList {
     }
 
     return $FileNameArray
+
 }
 
 function Get-FunctionCount {
@@ -247,6 +315,33 @@ function Get-ModuleFile {
 
 }
 
+function Get-ParsedContent {
+    <#
+        .DESCRIPTION
+        Description of TestScript
+
+        .SYNOPSIS
+        A detailed synopsis of the function of the script
+
+        .PARAMETER Content
+        A string containing the message
+
+        .EXAMPLE
+        Get-TestFunction -Message 'This is a test message'
+    #>
+    [CmdletBinding()]
+    [OutputType([System.Object[]])]
+    param (
+        [string]$Content
+    )
+
+    $ParserErrors = $null
+    $ParsedModule = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ParserErrors)
+
+    return $ParsedModule, ($ParserErrors.Count)
+
+}
+
 function Get-ParsedFile {
     <#
         .DESCRIPTION
@@ -281,33 +376,6 @@ function Get-ParsedFile {
     ($ParsedModule, $ParserErrorCount) = Get-ParsedContent -Content $fileContent
 
     return $ParsedModule, $ParserErrorCount
-
-}
-
-function Get-ParsedContent {
-    <#
-        .DESCRIPTION
-        Description of TestScript
-
-        .SYNOPSIS
-        A detailed synopsis of the function of the script
-
-        .PARAMETER Content
-        A string containing the message
-
-        .EXAMPLE
-        Get-TestFunction -Message 'This is a test message'
-    #>
-    [CmdletBinding()]
-    [OutputType([System.Object[]])]
-    param (
-        [string]$Content
-    )
-
-    $ParserErrors = $null
-    $ParsedModule = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ParserErrors)
-
-    return $ParsedModule, ($ParserErrors.Count)
 
 }
 
@@ -460,3 +528,4 @@ function Get-TokenMarker {
     return $token
 
 }
+
